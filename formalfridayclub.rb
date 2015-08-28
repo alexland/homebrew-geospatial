ed327a69e29bc47657c6bf1967574d992bb3bbde
alexland <doug.ybarbo@gmail.com>
2015-08-28 15:49:58 -0700

    test formula created

---
+++ formalfridayclub.rb
@@ -0,0 +1,12 @@
+
+class Formalfridayclub < Formula
+	homepage "https://github.com/alexland/formalfridayclub"
+	url "https://github.com/alexland/formalfridayclub/archive/1.0.0.tar.gz"
+	sha256 "ae8fd1b0e9e570d32462643ef4eaa16d1ee95f1dd164284c3cf318d83647c725"
+
+
+
+	def install
+		bin.install "formalfriday"
+	end
+end
